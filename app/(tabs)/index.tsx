4a76cf6b00da5ba9bb6ab22fc88ab560ea729cc3
tergalula <dries_lula@hotmail.fr>
2024-12-25 16:48:54 +0100

    la base du css avec flexbox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,47 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, Button, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Button,
+} from "react-native";
 
 const App = () => {
-
-  //Code Js ici 
-  let nom = "Lula"
-  var prenom = "Dries" 
-  const postnom = "Yekola"
-
-  const [nombre, setnombre] = useState(7)
-  const [name, setname] = useState("Brown")
-
   return (
     <ScrollView style={styles.container}>
-      
+      <Text style={styles.main}> Listes des produits</Text>
+
+      <View style={styles.pictureBox}>
+        <Image
+          style={styles.picture}
+          source={{
+            uri: "https://i.pinimg.com/736x/e2/cb/29/e2cb29cd11f0ea5513b4dc0cc9842087.jpg",
+          }}
+        />
+      </View>
+
       <View style={styles.box}>
-        <Text style={styles.title}>yekola {nombre} </Text>
+        <View style={styles.item1}> </View>
+        <View style={styles.item2}></View>
+        <View style={styles.item3}> </View>
+        <View style={styles.item4}></View>
       </View>
 
-      <View style={styles.box1}>
-        <Text style={styles.title}>Body {nom+' - '+postnom} </Text>
-        <Button style={styles.main} title='Valider' onPress={()=>setnombre(nombre + 10)}></Button>
-        <Text style={styles.buttonText} onPress={()=>setnombre(nombre + 20)}>Appuyer ici</Text>
+      <View style={styles.box}>
+        <View style={styles.item1}> </View>
+        <View style={styles.item2}></View>
+        <View style={styles.item3}> </View>
+        <View style={styles.item4}></View>
       </View>
 
-      <View style={styles.box2}>
-        <Text> Enter your Name please : </Text>
-        <TextInput style={styles.input} placeholder="Enter your name" onChangeText={(text)=>setname(text)}></TextInput>
-        <Text style={styles.title4}> My name is : {name} </Text>
+      <View style={styles.box}>
+        <View style={styles.item1}> </View>
+        <View style={styles.item2}></View>
+        <View style={styles.item3}> </View>
+        <View style={styles.item4}></View>
       </View>
-
-      <View style={styles.box3}>
-        <Text style={styles.title}>Body-3 {prenom} {postnom} </Text>
-      </View>
-
     </ScrollView>
   );
 };
@@ -41,86 +49,65 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#d0d3d4",
-    padding: "auto",
-    width: "auto",
-  },
-  input: {
-    borderColor: "#000",
-    borderWidth: 1,
+    backgroundColor: "#C41C57",
     padding: 10,
-    margin: 10,
-    backgroundColor: "#fff",
-    width: "auto"
-  },
-  title4: {
-    color: "#000",
-    fontSize: 28,
-    margin: "auto",
-    fontWeight: "bold",
-    textAlign: "center",
-    textTransform: "capitalize",
-  },
-  title: {
-    color: "#fff",
-    fontSize: 28,
-    // marginTop: 40,
-    margin: "auto",
-    fontWeight: "bold",
-    textAlign: "center",
-    textTransform: "uppercase",
-  },
-  buttonText: {
-    backgroundColor: '#1e90ff', // Couleur de fond du bouton
-    paddingVertical: 15,        // Espace vertical
-    paddingHorizontal: 30,      // Espace horizontal
-    borderRadius: 30,           // Bordure arrondie
-    elevation: 5,               // Ombre pour Android
-    shadowColor: '#000',        // Ombre pour iOS
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    width: "40%",
-    textAlign: "center",
-    margin: "auto"
   },
   box: {
-    backgroundColor: "#900C3F",
-    width: "100%",
+    backgroundColor: "white",
+    height: 400,
+    width: "auto",
+    margin: 10,
+    // padding: 20,
+    // marginRight : 20,
+
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  item1: {
+    backgroundColor: "blue",
     height: 100,
-    padding: "auto",
-    shadowColor: '#000',
-    shadowRadius: 20
+    width: 50,
   },
-  box1: {
-    backgroundColor: "#edbb99",
-    marginTop: 10,
-    width: "98%",
-    borderRadius: 10,
-    height: 200,
-    margin: "auto",
-    shadowColor: '#000',
-    shadowRadius: 20
+
+  item2: {
+    backgroundColor: "red",
+    height: 100,
+    width: 50,
   },
-  box2: {
-    backgroundColor: "#edbb99",
-    marginTop: 10,
-    width: "98%",
-    borderRadius: 10,
-    height: 200,
-    margin: "auto",
-    shadowColor: '#000',
-    shadowRadius: 20
+  item3: {
+    backgroundColor: "yellow",
+    height: 100,
+    width: 50,
   },
-  box3: {
-    backgroundColor: "#edbb99",
-    marginTop: 10,
-    width: "98%",
-    borderRadius: 10,
-    height: 200,
-    margin: "auto",
-    shadowColor: '#000',
-    shadowRadius: 20
+  item4: {
+    backgroundColor: "green",
+    height: 100,
+    width: 50,
+  },
+  main: {
+    backgroundColor: "white",
+    fontWeight: 900,
+    fontSize: 30,
+    textAlign: "center",
+    color: "blue",
+    textTransform: "capitalize",
+  },
+  pictureBox: {
+    backgroundColor: "blue",
+    width: "auto",
+    height: 300,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+
+  picture: {
+    // width: 150,
+    // height: 150,
+    width: "auto",
+    height: "100%",
+    resizeMode: "stretch",
   },
 });
 
